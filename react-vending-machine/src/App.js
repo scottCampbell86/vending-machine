74419e0e6ec8505cbebd58018a3954d7106ace0a
Scott Campbell <stc9486@gmail.com>
2020-04-01 15:26:23 -0700

    routed and linked

--- react-vending-machine/src/App.js
+++ react-vending-machine/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import  VendingMachine from './JS/VendingMachine';
 import Chips from './JS/Chips';
 import Sardines from './JS/Sardines';
--- react-vending-machine/src/JS/VendingMachine.js
+++ react-vending-machine/src/JS/VendingMachine.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
+// import Chips from './JS/Chips';
+// import Sardines from './JS/Sardines';
+// import Soda from './JS/Soda'
 import '../CSS/VendingMachine.css'
 
 
@@ -7,6 +11,20 @@ class VendingMachine extends Component {
     return (
       <div>
         <h1>What Up!</h1>
+        <h3>this is a REACT *demo* vending machine</h3>
+        <h4>what would you like?</h4>
+        <div>
+          <section className="LINKS">
+            <h4>
+              <Link to='/soda'>Soda</Link></h4>
+            <h4>
+              <Link to='/Chips'>Chips</Link></h4>
+            <h4>
+              <Link to='/sardines'>Sardines</Link></h4>
+          </section>
+        </div>
+       
+          
       </div>
     );
   }
