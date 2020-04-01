fcf52c36da20bdf56b7354083659c653c366e8af
Scott Campbell <stc9486@gmail.com>
2020-04-01 15:10:21 -0700

    routes

--- react-vending-machine/src/App.js
+++ react-vending-machine/src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import  VendingMachine from './JS/VendingMachine';
-import './App.css';
 import Chips from './JS/Chips';
 import Sardines from './JS/Sardines';
 import Soda from './JS/Soda'
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <VendingMachine />
-          <Chips />
-          <Soda />
-          <Sardines />
+        <Router>
+          <Switch>
+            <Route exact path='/' render={() => <VendingMachine />} />
+            <Route exact path='/chips' render={() => <Chips />} />
+            <Route exact path='/soda' render={() => <Soda />} />
+            <Route exact path='/sardines' render={() => <Sardines />} />
+          </Switch>
+        </Router>
       </div> 
     );
   }
